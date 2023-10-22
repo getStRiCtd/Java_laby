edf5af9ee4d8c1aac22881f8d3077378cd5bccb2
Danil <DADemidenko@stud.kantiana.ru>
2023-10-22 15:31:29 +0200

    Update CourseDataReader

---
+++ src/main/java/lab2/registration/reader/CourseDataReader.java
@@ -0,0 +1,24 @@
+package lab2.registration.reader;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lab2.registration.model.CourseInfo;
+import lab2.registration.model.CourseInstance;
+
+
+import java.io.File;
+import java.io.IOException;
+
+public class CourseDataReader {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public CourseInfo[] readCourseInfo() throws IOException {
+        return objectMapper
+                .readValue(new File("src/main/resources/courseInfos.json"), CourseInfo[].class);
+    }
+    public CourseInstance[] readCourseInstance() throws IOException {
+        return objectMapper
+                .readValue(new File("src/main/resources/courseInstances.json"), CourseInstance[].class);
+    }
+
+}
